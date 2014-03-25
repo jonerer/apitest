ceafa82110452be989d0ffb9a027143baf25cba0
Joooom <jonma752@student.liu.se>
2014-03-25 20:02:43 +0100

    tests are even more super awesome now

--- lib.js
+++ lib.js
@@ -6,8 +6,10 @@ var supertest = require('supertest'),
 
 Test.prototype.api = function fniss(a, b) {
   // TODO: also assert on the HTTP header code
-  var code;
+  var code,
+    isObject;
 
+  isObject = 'object' === typeof a
   if ('object' === typeof a) {
     code = a.code
   } else if ('number' === typeof a) {
@@ -22,7 +24,14 @@ Test.prototype.api = function fniss(a, b) {
         return res.body.status === 'fail' ? null : 'Expected response body to have status "fail"'
       })    
       this._asserts.push(function(res) {
-        return res.body.code === code ? null : 'Expected response.code to be ' + code + '. Actual: ' + res.body.code
+        var exp, act, msg = '';
+        if (res.body.code !== code) {
+          if (isObject) {
+            msg += 'Expected response to be "' + a.response + '". Actual: "' + res.body.response + '"\n'
+          }
+          msg += 'Expected code: ' + code + '. Actual code: ' + res.body.code
+          return msg
+        }
       })
   }
 
--- test.js
+++ test.js
@@ -2,23 +2,30 @@ var apisend = require('apisend')
 var apitest = require('./lib')
 
 var request = require('supertest')
+var assert = require('assert')
+require('should')
 
 // TODO: describe error modes, and when the asserts should fail
 describe('apisend basic', function() {
   var app,
-    closer;
+    closer,
+    errors = {
+      UNAUTH: {
+        http: 401,
+        code: 1,
+        response: 'You are not authorized'
+      },
+      UNUSED: {
+        http: 401,
+        code: 2,
+        response: 'This error is not used'
+      }
+    };
 
   before(function(done) {
     var express = require('express')
     app = express()
 
-    var errors = {
-      UNAUTH: {
-        http: 401,
-        code: 1,
-        response: 'You are not authorized'
-      }
-    }
     app.use(apisend(errors))
     app.get('/', function(req, res) {
       res.apisend(0)
@@ -50,6 +57,25 @@ describe('apisend basic', function() {
     request(app).get('/code1').api(1, done)
   })
 
+  it('should accept code 1, but sent in as an object', function(done) {
+    request(app).get('/code1').api(errors.UNAUTH, done)
+  })
+
+  it('should not accept code 2 (as an object)', function(done) {
+    request(app).get('/code1').api(errors.UNUSED).end(function (err, res) {
+      err.should.Error
+      err.toString().should.eql('Error: Expected response to be "This error is not used". Actual: "You are not authorized"\nExpected code: 2. Actual code: 1')
+      done()
+    })
+  })
+
+  it('should not accept code 2 (as a number)', function(done) {
+    request(app).get('/code1').api(2).end(function(err, res) {
+      err.should.Error
+      err.toString().should.eql('Error: Expected code: 2. Actual code: 1')
+      done()
+    })
+  })
 
   after(function(done) {
     closer.close(done)
